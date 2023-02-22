b30dd76bb400b1859fffc81963c2f67fc6e25197
Denis <golotaistrov@ukr.net>
2023-02-22 12:41:16 +0200

    UI of my Site

---
+++ AboutProject.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+    <link href="default.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
+
+</head>
+
+<body>
+    <div id="wrapper">
+
+        <?php
+        include('Headwrapper.php');
+        ?>
+
+        <div id="page" class="container">
+
+            <div class="second-page">
+
+                <!-- title phrase -->
+                <div class="title-phrase">
+                    <h1>The path to becoming a QA</h1>
+                </div>
+
+                <!-- about top  -->
+                <div class="about-top">
+                    <p>
+                        <b>Hello, I'm Denis</b>, the creator of this beautiful site. I want to immediately emphasize
+                        that this site was created because of the desire of the developer to learn something new.
+                        Creating a
+                        website made me think more broadly, analyze the situation more, I also gained practical
+                        experience in creating a website, studied new technologies, and, not least, I consolidated
+                        the theory of product testing. I am going to develop directly as a <b>QA engineer</b> and one of
+                        the
+                        goals of creating a site is to transfer all my knowledge and make it accessible to everyone. I'm
+                        not a professional developer, so you might find bugs by accident. If suddenly you find one,
+                        contact me, I will leave the <u>contact details</u> on this page.
+                    </p>
+                    <p>
+                        If we talk about the <b>functionality of the website</b>, then this is a website on which the
+                        user
+                        can write down his goals that he wants to fulfill. On the <a
+                            href="index.php"><b>Homepage</b></a>, you
+                        can see the earliest active goal and the most recently completed goal. Also on this page, the
+                        user can view the
+                        latest news from the world of technology. Goals that the user has not yet completed or canceled
+                        are displayed in the <a href="CurrentGoals.php"><b>Current Goals</b></a> section, and goals that
+                        the user has completed are
+                        displayed in the <a href="CompletedGoals.php"><b>Completed Goals</b></a> section. In the <a
+                            href="Education.php"><b>Educational Material</b></a> section, the user can
+                        see information that, according to the developer of this site, a QA engineer needs to know. And
+                        finally, the section <a href="AboutProject.php"><b>About Project & Contact</b></a> - the section
+                        on the page of which you are
+                        now, here you can find out information about the developer, the project and its goals. Below are
+                        the goals that the developer set for <u>himself</u>.
+                    </p>
+                </div>
+
+
+                <section id="about-section">
+
+                    <!-- about left  -->
+                    <div class="about-left">
+                        <img src="images/Keyboard.png" alt="About Img" />
+                    </div>
+
+                    <!-- about right  -->
+                    <div class="about-right">
+                        <br>
+                        <h3>Project goals:</h3>
+                        <ul>
+                            <li>
+                                <p>&ndash; Find out how the websites works and what they consist of;</p>
+                            </li>
+                            <li>
+                                <p>&ndash; Explore new technologies that are related to the development, testing,
+                                    storage and processing of data;</p>
+                            </li>
+                            <li>
+                                <p>&ndash; Get practical skills in creating a website;</p>
+                            </li>
+                            <li>
+                                <p>&ndash; Consolidate knowledge about testing and share it with others;</p>
+                            </li>
+                            <li>
+                                <p>&ndash; Expand the boundaries of creative thinking;</p>
+                            </li>
+                        </ul>
+                        <p>
+                        </p>
+                        <div class="address">
+                            <ul>
+                                <li>
+                                    <span class="address-logo">
+                                        <i class="fas fa-paper-plane"></i>
+                                    </span>
+                                    <p>Address</p>
+                                    <span class="saprater">:</span>
+                                    <p>Kharkiv, Ukraine</p>
+                                </li>
+                                <li>
+                                    <span class="address-logo">
+                                        <i class="fas fa-phone-alt"></i>
+                                    </span>
+                                    <p>Phone &#8470;</p>
+                                    <span class="saprater">:</span>
+                                    <p>+38(012)345-67-89</p>
+                                </li>
+                                <li>
+                                    <span class="address-logo">
+                                        <i class="far fa-envelope"></i>
+                                    </span>
+                                    <p>Email ID</p>
+                                    <span class="saprater">:</span>
+                                    <p>my_email_here@gmail.com</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+
+                <div class="expertise">
+                    <h3>My Expertise</h3>
+                    <ul>
+                        <li>
+                            <span class="expertise-logo">
+                                <i class="fab fa-html5"></i>
+                            </span>
+                            <p>HTML</p>
+                        </li>
+                        <li>
+                            <span class="expertise-logo">
+                                <i class="fab fa-css3-alt"></i>
+                            </span>
+                            <p>CSS</p>
+                        </li>
+                        <li>
+                            <span class="expertise-logo">
+                                <i class="fab fa-node-js"></i>
+                            </span>
+                            <p>Java Script</p>
+                        </li>
+                        <li>
+                            <span class="expertise-logo">
+                                <i class="fab fa-php"></i>
+                            </span>
+                            <p>PHP</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div id="footer" class="container">
+            <hr />
+            <p>&copy; Not found. Write here something interesting or useful</p>
+        </div>
+        <!--Bottom indent-->
+        <div class="indent"></div>
+    </div>
+</body>
+
+</html>
---
+++ Headwrapper.php
@@ -0,0 +1,35 @@
+<div id="header-wrapper">
+
+	<div id="header" class="container">
+		<div id="logo">
+
+			<a href="index.php"><img src="images/logo-QA.png" /></a>
+
+		</div>
+		<div id="social">
+			<ul class="contact">
+				<li><a href="https://twitter.com/Kapuchinka69"><img src="images/Twitter.png"
+							width="30" /><span>Twitter</span></a></li>
+				<br>
+				<li><a href="https://www.instagram.com/dench1kman/"><img src="images/Instagram.png"
+							width="30" /><span>Instagram</span></a></li>
+				<br>
+				<li><a href="https://t.me/bombinchik"><img src="images/Telegram.png"
+							width="30" /><span>Telegram</span></a></li>
+				<br>
+				<li><a href="https://github.com/Dench1kman"><img src="images/Github.png"
+							width="30" /><span>Github</span></a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div id="menu" class="container">
+		<ul>
+			<li><a href="index.php">Homepage</a></li>
+			<li><a href="CurrentGoals.php">Current Goals</a></li>
+			<li><a href="ComplitedGoals.php">Complited Goals</a></li>
+			<li><a href="Education.php">Educational Material</a></li>
+			<li><a href="AboutProject.php">About Project & Contact</a></li>
+		</ul>
+	</div>
+</div>
