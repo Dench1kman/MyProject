5dbee03f02150c1af1cc70a9ebc665c5300b5341
Denis <golotaistrov@ukr.net>
2023-02-22 12:26:18 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<!--
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+-->
+	<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+	<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+
+<body>
+	<style>
+		body {
+			background: url(images/Background-8.png) 0 0/1900px no-repeat fixed;
+		}
+	</style>
+	<div id="wrapper">
+
+		<?php
+		include('Headwrapper.php');
+		?>
+
+		<div id="page" class="container">
+			<div class="second-page">
+				<!-- about top  -->
+				<div class="about-top">
+					<h1>Homepage</h1>
+				</div>
+			</div>
+		</div>
+		<div id="footer" class="container">
+			<hr />
+			<p>&copy; Not found. Write here something interesting or useful</p>
+		</div>
+		<!--Bottom indent-->
+		<div class="indent"></div>
+	</div>
+
+</body>
+
+</html>
